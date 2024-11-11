d06c1619021308d393384164b9c6d83f0ab0b2c1
hiroki331 <qyouhui26@gmail.com>
2024-11-11 22:26:21 +0900

    メディアクエリをまとめる

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const { src, dest, watch, series, parallel } = require('gulp');
-const sass = require('gulp-sass')(require('sass')); // ここでDart Sassを指定
+const sass = require('gulp-sass')(require('sass'));
 const plumber = require("gulp-plumber");
 const notify = require("gulp-notify");
 const postcss = require("gulp-postcss");
+const mqpacker = require('css-mqpacker');  
 const autoprefixer = require("autoprefixer");
-const sourcemaps = require('gulp-sourcemaps'); // sourcemapsの読み込み
+const sourcemaps = require('gulp-sourcemaps');
 const sassGlob = require("gulp-sass-glob");
+const rename = require("gulp-rename");
+const cleanCSS = require("gulp-clean-css");
 
 const srcPath = {
   css: './scss/*.scss',
@@ -26,13 +29,20 @@ const cssSass = (done) => {
       outputStyle: "expanded"
     }))
     // .pipe(sourcemaps.write("./"))
-    .pipe(postcss([autoprefixer({overrideBrowserslist: ['last 2 versions']})]))
+    .pipe(postcss([autoprefixer({overrideBrowserslist: ['last 2 versions']}), mqpacker()]))
+    .pipe(dest(destPath.css)) // 圧縮前のCSSファイルの出力
+    .pipe(cleanCSS()) // CSSを圧縮
+    .pipe(
+      rename({
+        extname: '.min.css' //.min.cssの拡張子にする
+      })
+    )
     .pipe(dest(destPath.css))
     .on('end', done); // タスクの終了を通知
 }
 
 const watchSassFiles = () => {
-  watch(srcPath.css, cssSass);
+  watch('./scss/**/*.scss', cssSass);
 }
 
 exports.dev = watchSassFiles;
